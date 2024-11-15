96cefed5f2bdf511c8d7552ca862b247cc79dc1f
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-11-15 21:41:16 +0700

    Added Brian Resource

--- ai-agents/src/app.module.ts
+++ ai-agents/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BrianModule } from './brian/brian.module';
 
 @Module({
-  imports: [],
+  imports: [BrianModule],
   controllers: [AppController],
   providers: [AppService],
 })
