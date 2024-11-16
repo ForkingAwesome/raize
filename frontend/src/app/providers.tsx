f61a1a17fea0ee7e50a2f049d72408835a8d68fb
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-11-16 21:14:02 +0700

    Minor changes

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -3,10 +3,7 @@
 import { useState, type ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {
-  DynamicContextProvider,
-  DynamicWidget,
-} from "@dynamic-labs/sdk-react-core";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { FlowWalletConnectors } from "@dynamic-labs/flow";
