a66e9d383bdf7ea907b23281e2ae7161c9b1098e
Manav <manav18gadhiya@gmail.com>
2024-11-17 07:03:46 +0700

    add: cards loading animation

--- frontend/src/components/ui/CardsAndChips.tsx
+++ frontend/src/components/ui/CardsAndChips.tsx
@@ -1,15 +1,40 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const CardsAndChips = () => {
   return (
-    <div className="relative flex flex-col justify-center items-center h-[400px] space-y-[-10px]">
-      <Image
-        src="/loading_cards.png"
-        alt="Loading Cards"
-        width={280}
-        height={150}
-      />
-      <Image src="/chips.png" alt="chips" width={120} height={100} />
+    <div className="relative flex flex-col justify-center items-center h-[400px]">
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 20, opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 15,
+          duration: 3,
+        }}
+      >
+        <Image
+          src="/loading_cards.png"
+          alt="Loading Cards"
+          width={280}
+          height={150}
+          className="relative z-10"
+        />
+      </motion.div>
+      <motion.div
+        initial={{ x: 65, y: 100, opacity: 0 }}
+        animate={{ x: 65, y: -85, opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 15,
+          duration: 3,
+          delay: 0.1,
+        }}
+      >
+        <Image src="/chips.png" alt="chips" width={120} height={100} />
+      </motion.div>
     </div>
   );
 };
